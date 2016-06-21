2b3873e51c1bf047932d83553be339fd4fcf4341
MarchCastro <march.castrof@gmail.com>
2016-06-21 01:07:12 -0500

    Subiendo Alta Usuario

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); //VISTAS VAN EN VIEWS
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
